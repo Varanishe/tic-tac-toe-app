1512175bdfc24a2571b931583ffe6d5c3956b288
pyjioh <pyjioh@db38e329-a2b1-14ce-4545-633a358d27bd>
2010-09-02 06:43:34 +0000

    add test

--- tic-tac-toe-app/src/pyjioh/apps/tictactoe/tests/TicTacToeModelTest.java
+++ tic-tac-toe-app/src/pyjioh/apps/tictactoe/tests/TicTacToeModelTest.java
@@ -16,7 +16,7 @@ public class TicTacToeModelTest {
 	private TicTacToeModel model = TicTacToeModel.getInstance();
 	
 	@Test
-	public void test_NewGame() {
+	public void test_NewGame() { 
 		model.setDifficulty(TicTacToeModel.HARD_DIF);
 		model.newGame();
 		assertTrue(TicTacToeModel.EMPTY == model.getWinner());
@@ -28,9 +28,9 @@ public class TicTacToeModelTest {
 		model.setDifficulty(TicTacToeModel.HARD_DIF);
 		model.newGame();
 		model.doMove(0, 0);
-		model.doMove(2, 0);
-		model.doMove(1, 2);
+		model.doMove(0, 2);
 		model.doMove(2, 1);
+		model.doMove(1, 2);
 		model.doMove(1, 0);
 		assertTrue(model.getState() == TicTacToeModel.STATE_DRAW);
 	}
@@ -39,14 +39,58 @@ public class TicTacToeModelTest {
 	public void test_HardDraw2() {
 		model.setDifficulty(TicTacToeModel.HARD_DIF);
 		model.newGame();
-
+		model.doMove(2, 1);
+		model.doMove(2, 0);
+		model.doMove(0, 0);
+		model.doMove(1, 2);
+		model.doMove(0, 1);
+		assertTrue(model.getState() == TicTacToeModel.STATE_DRAW);
 	}
 
 	@Test
 	public void test_HardDraw3() {
 		model.setDifficulty(TicTacToeModel.HARD_DIF);
 		model.newGame();
+		model.doMove(2, 1);
+		model.doMove(2, 2);
+		model.doMove(0, 2);
+		model.doMove(1, 0);
+		model.doMove(0, 1);
+		assertTrue(model.getState() == TicTacToeModel.STATE_DRAW);
+	}
 
+	@Test
+	public void test_HardLose1() {
+		model.setDifficulty(TicTacToeModel.HARD_DIF);
+		model.newGame();
+		model.doMove(0, 0);
+		model.doMove(0, 1);
+		model.doMove(1, 0);
+		assertTrue(model.getState() == TicTacToeModel.STATE_WIN);
+		assertTrue(model.getWinner() == TicTacToeModel.NOUGHT);
+	}
+
+	@Test
+	public void test_HardLose2() {
+		model.setDifficulty(TicTacToeModel.HARD_DIF);
+		model.newGame();
+		model.doMove(2, 2);
+		model.doMove(0, 0);
+		model.doMove(2, 1);
+		assertTrue(model.getState() == TicTacToeModel.STATE_WIN);
+		assertTrue(model.getWinner() == TicTacToeModel.NOUGHT);
+	}
+
+	@Test
+	public void test_HardLose3() {
+		model.setDifficulty(TicTacToeModel.HARD_DIF);
+		model.newGame();
+		model.doMove(0, 2);
+		model.doMove(1, 2);
+		model.doMove(0, 0);
+		model.doMove(1, 0);
+		assertTrue(model.getState() == TicTacToeModel.STATE_WIN);
+		assertTrue(model.getWinner() == TicTacToeModel.NOUGHT);
 	}
 
 }
